2738a1ce7d8ac149c0dbf8ed259af9f841adf0d9
Christina O'Donnell <christina.odonnell@protonmail.com>
2019-11-21 04:48:38 +0000

    editor.h

---
+++ editor.h
@@ -0,0 +1,15 @@
+/* See LICENSE for license details. */
+
+#include "cdoedit.h"
+
+void changeindent(const Arg *);
+void deletechar(const Arg *);
+void deleteword(const Arg *);
+void navchar(const Arg *);
+void navdocument(const Arg *);
+void navline(const Arg *);
+void navpage(const Arg *);
+void navparagraph(const Arg *);
+void navrow(const Arg *);
+void navword(const Arg *);
+void newline(const Arg *);
